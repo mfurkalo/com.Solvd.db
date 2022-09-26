f345aa825416400ad659ec052d8fb047300981d7
Maksym <maximkack@gmail.com>
2022-09-26 08:50:08 -0400

    Json read/write with Jackson

---
+++ src/main/java/core/JacksonReadWrite.java
@@ -0,0 +1,47 @@
+/*
+ *  Copyright (c)
+ *  Maksym Furkalo,
+ *  work with DB using the DAO pattern,
+ *  use for free
+ */
+
+package core;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import utils.interfaces.IJacksonReadWrite;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class JacksonReadWrite implements IJacksonReadWrite {
+    static Logger log = LogManager.getLogger(JacksonReadWrite.class.getName());
+
+    @Override
+    public String unmarshal(Object object) {
+        ObjectMapper om = new ObjectMapper();
+        String json = null;
+        try {
+            json = om.writerWithDefaultPrettyPrinter().writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            log.log(Level.FATAL, "The object failed to unmarshal");
+            log.log(Level.FATAL, e.getMessage(), e);
+        }
+        return json;
+    }
+
+    @Override
+    public <T> T marshall(String path, Class<?> T) {
+        ObjectMapper om = new ObjectMapper();
+
+        try {
+            return (T) om.readValue(new FileReader(path), T);
+        } catch (IOException e) {
+            log.log(Level.FATAL, e.getMessage(), e);
+            return (T) T;
+        }
+    }
+}
